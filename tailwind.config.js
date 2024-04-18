737be5e90fef07b36e7bd19c1d95e99545efcfd5
Kaapo.Studio <contact@kaapo.studio>
2024-04-18 10:37:26 +0300

    build fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const {nextui} = require('@nextui-org/theme');
+import { nextui } from "@nextui-org/theme";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -33,27 +33,5 @@ module.exports = {
     },
   },
   darkMode: "class",
-  plugins: [nextui({
-      // themes: {
-      //   light: {
-      //     colors: {
-      //       background: #FFFFFF, // or DEFAULT
-      //       foreground: #11181C, // or 50 to 900 DEFAULT
-      //       content1: #FFFFFF, // or
-      //       primary: {
-      //         //... 50 to 900
-      //         foreground: #FFFFFF,//         DEFAULT: #84cc16,//         50: #f7fee7,//         100: #ecfccb,//         200: #d9f99d,//         300: #bef264,//         400: #a3e635,//         500: #84cc16,//         600: #65a30d,//         700: #4d7c0f,//         800: #3f6212,//         900: #365314,//       }, // ... rest of the colors
-      //     },//   },//   dark: {
-      //     colors: {
-      //       background: #000000, // or DEFAULT
-      //       foreground: #ECEDEE, // or 50 to 900 DEFAULT
-      //       content1: #000000, // or
-      //       primary: {
-      //         //... 50 to 900
-      //         foreground: #FFFFFF,//         DEFAULT: #84cc16,//         50: #f7fee7,//         100: #ecfccb,//         200: #d9f99d,//         300: #bef264,//         400: #a3e635,//         500: #84cc16,//         600: #65a30d,//         700: #4d7c0f,//         800: #3f6212,//         900: #365314,//       },//     }, // ... rest of the colors
-      //   }, // mytheme: {
-      //   //   // custom theme
-      //   //   extend: dark,//   //   colors: {
-      //   //     primary: {
-      //   //       DEFAULT: #84cc16,//   //       foreground: #000000,//   //     },//   //     focus: #84cc16,//   //   },//   // },// },}),nextui()],
+  plugins: [nextui()],
 };
